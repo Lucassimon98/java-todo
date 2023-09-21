0246c869b8ddeb48543a576dbba2ba1463383d10
Lucas Henrique Simon <lucassimon98@gmail.com>
2023-09-21 18:41:31 -0300

    Criação do projeto e estruturado

---
+++ src/main/java/br/com/lucassimon/javatodo/JavaTodoApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucassimon.javatodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTodoApplication.class, args);
+	}
+
+}
